5a212d85cf1888a4e4152ccebe59c22fc4ba16d1
maeda.nanako <maeda.nanako0301@gmail.com>
2026-01-29 09:02:14 +0900

    add schedule for nightly run

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,6 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// ★この3行のコメントアウトを解除
 import dotenv from 'dotenv';
 import path from 'path';
 dotenv.config({ path: path.resolve(__dirname, '.env') });
